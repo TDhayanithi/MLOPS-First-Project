e9d3d73dbc2cfa01a8a8aca0233ef3d3b265a02a
Dhayanithi <dhayanithi.t14@gmail.com>
2024-02-27 21:23:00 +0530

    Structure_updated

--- template.py
+++ template.py
@@ -13,6 +13,7 @@ list_of_files = [
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
     "src/utils/__init__.py",
+    "src/utils/utils.py",
     "src/logger/logging.py",
     "src/exception/exception.py",
     "tests/unit/__init__.py",
